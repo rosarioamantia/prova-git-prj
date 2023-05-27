839a62e8c19e7a54613a2b174734f40fb4b2c2cc
rosar <rosario.amantia.dev@gmail.com>
2023-05-27 13:12:03 +0200

    first commit

---
+++ app/src/main/java/com/rosario/prova_git_prj/MainActivity.java
@@ -0,0 +1,15 @@
+package com.rosario.prova_git_prj;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        System.out.println("ciao");
+    }
+}
